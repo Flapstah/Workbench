c87ef69b5a84f7ace7f60cdf5e6e81a69fbf6bc3
andyc <andyc@crytek.com>
2010-07-19 18:25:12 +0100

    system clock test

--- ConsoleUnitTest/ConsoleUnitTest.cpp
+++ ConsoleUnitTest/ConsoleUnitTest.cpp
@@ -3,8 +3,17 @@
 
 #include "stdafx.h"
 
+#include <iostream>
+
+#include "common/itime.h"
+
 int _tmain(int argc, _TCHAR* argv[])
 {
+	const engine::ISystemClock* pSystemClock = engine::GetSystemClock();
+
+	std::cout << pSystemClock->GetLocalDateString() << std::endl;
+	std::cout << pSystemClock->GetLocalTimeString() << std::endl;
+
 	return 0;
 }
 
