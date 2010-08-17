f1cb4aff85dad9b08c46b94ad6262a68094cf417
andyc <andyc@crytek.com>
2010-08-17 12:06:17 +0100

    extended interface

--- engine/independent/common/ifilesystem.h
+++ engine/independent/common/ifilesystem.h
@@ -6,6 +6,10 @@
 #define COMPANY_NAME "MyCompanyName"
 #define PROJECT_NAME "MyProjectName"
 
+//#define FILE_SYSTEM_EXTENDED
+// TODO: Need to flesh out extended file system functions
+// TODO: Need some sort of enumerator to walk file/directories
+
 //==============================================================================
 
 namespace engine
@@ -53,12 +57,21 @@ namespace engine
 
 		virtual eFileSystemError	DirectoryExists(TCHAR* path) = 0;
 		virtual eFileSystemError	CreateDirectory(TCHAR* path) = 0;
+#if defined(FILE_SYSTEM_EXTENDED)
+		virtual eFileSystemError	CopyDirectory(TCHAR* sourcePath, TCHAR* destinationPath) = 0;
+		virtual eFileSystemError	MoveDirectory(TCHAR* sourcePath, TCHAR* destinationPath) = 0;
 		virtual eFileSystemError	DeleteDirectory(TCHAR* path, bool force) = 0;
+#endif
 
 		//==========================================================================
 
 		virtual eFileSystemError	FileExists(TCHAR* name) = 0;
 		virtual eFileSystemHandle	OpenFile(TCHAR* name, TCHAR* mode) = 0;
+#if defined(FILE_SYSTEM_EXTENDED)
+		virtual eFileSystemError	CopyFile(TCHAR* sourceName, TCHAR* destinationName) = 0;
+		virtual eFileSystemError	MoveFile(TCHAR* sourceName, TCHAR* destinationName) = 0;
+		virtual eFileSystemError	DeleteFile(TCHAR* name, bool force) = 0;
+#endif
 		virtual size_t						Read(eFileSystemHandle handle, void* pBuffer, size_t bufferSize, size_t itemSize, size_t itemCount) = 0;
 		virtual size_t						Write(eFileSystemHandle handle, const void* pBuffer, size_t itemSize, size_t itemCount) = 0;
 		virtual size_t						Print(eFileSystemHandle handle, const TCHAR* format, ...) = 0;
