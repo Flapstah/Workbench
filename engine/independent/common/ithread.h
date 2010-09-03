fb9df1d17c4606624fbcdf7be6edb3316311ca54
andyc <andyc@crytek.com>
2010-09-03 18:03:16 +0100

    added mutex

---
+++ engine/independent/common/ithread.h
@@ -0,0 +1,60 @@
+#if !defined(__ITHREAD_H__)
+#define __ITHREAD_H__
+
+//==============================================================================
+
+namespace engine
+{
+	//============================================================================
+	// CMutex - don't use this templated class directly, use 'mutex' type instead
+	//============================================================================
+	template<typename _mutex>
+	class CMutex
+	{
+	public:
+		CMutex(void)												{	Platform_CreateMutex();									}
+		~CMutex(void)												{	Platform_DestroyMutex();								}
+
+		void Lock(void)											{ Platform_WaitForMutex(0);								}
+		void Unlock(void)										{	Platform_ReleaseMutex();								}
+		bool TryLock(uint32 timeout)				{ return Platform_WaitForMutex(timeout);	}
+
+	protected:
+		void Platform_CreateMutex(void);
+		void Platform_DestroyMutex(void);
+
+		bool Platform_WaitForMutex(uint32 timeout);
+		void Platform_ReleaseMutex(void);
+
+	protected:
+		_mutex m_mutex;
+
+	private:
+	}; // End [class CMutex]
+
+	//============================================================================
+
+#if defined(WIN32) || defined(WIN64)
+	typedef CMutex<HANDLE> mutex;
+#endif
+
+	//============================================================================
+
+	class CAutoMutexLock
+	{
+	public:
+		CAutoMutexLock(mutex& autoMutex) : m_autoMutex(autoMutex)	{	m_autoMutex.Lock();		}
+		~CAutoMutexLock(void)																			{	m_autoMutex.Unlock();	}
+
+	private:
+		PREVENT_CLASS_COPY(CAutoMutexLock);
+
+	private:
+		mutex& m_autoMutex;
+	};
+	//============================================================================
+} // End [namespace engine]
+
+//==============================================================================
+#endif // End [!defined(__ITHREAD_H__)]
+// [EOF]
---
+++ engine/windows/kernel/thread/platform_thread.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+
+#include "common/ithread.h"
+
+//==============================================================================
+
+namespace engine
+{
+	//============================================================================
+
+	//----------------------------------------------------------------------------
+	// There needs to be an explicit instantiation of this typed mutex in order
+	// that the platform function definitions can be separated from the
+	// declarations in the header.  Implicit instantiation would require the
+	// function definitions to be with the declaration. 
+	//----------------------------------------------------------------------------
+	template class CMutex<HANDLE>;
+
+	//============================================================================
+
+	template<typename _mutex> void CMutex<_mutex>::Platform_CreateMutex(void)
+	{
+		m_mutex = CreateMutex(NULL, FALSE, NULL);
+	}
+
+	//============================================================================
+
+	template<typename _mutex> void CMutex<_mutex>::Platform_DestroyMutex(void)
+	{
+		CloseHandle(m_mutex);
+	}
+
+	//============================================================================
+
+	template<typename _mutex> bool CMutex<_mutex>::Platform_WaitForMutex(uint32 timeout)
+	{
+		return (WaitForSingleObject(m_mutex, timeout) != WAIT_TIMEOUT);
+	}
+
+	//============================================================================
+
+	template<typename _mutex> void CMutex<_mutex>::Platform_ReleaseMutex(void)
+	{
+		ReleaseMutex(m_mutex);
+	}
+
+	//============================================================================
+} // End [namespace engine]
+
+//==============================================================================
+// [EOF]
