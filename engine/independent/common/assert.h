d17493e7842269da70c47ffe19d1390d3762dfbd
Flapstah <flapstah@gmail.com>
2010-11-11 22:36:23 +0000

    fixed assert

--- engine/independent/common/assert.h
+++ engine/independent/common/assert.h
@@ -18,15 +18,19 @@ namespace engine
 #define assert(_condition_) \
 	if (!(_condition_)) \
 	{ \
-		LogAssert((_TEXT("%s(%i): [Assert] condition (%s)"), _TEXT(__FILE__), __LINE__, _TEXT(#_condition_))); \
+		LogAssert(_TEXT("%s(%i): condition (%s)"), _TEXT(__FILE__), __LINE__, _TEXT(#_condition_)); \
 		DebugBreak(); \
 	}
 
 #define assertf(_condition_, ...) \
 	if (!(_condition_)) \
 	{ \
-		LogAssert((_TEXT("%s(%i): [Assert] condition (%s)"), _TEXT(__FILE__), __LINE__, _TEXT(#_condition_))); \
+		engine::ILogFile::eBehaviourFlag behaviourFlags = engine::g_AssertLog->GetBehaviours(); \
+		engine::g_AssertLog->TurnOffBehaviours(engine::ILogFile::eBF_ForceInsertNewline); \
+		LogAssert(_TEXT("%s(%i): condition (%s) : "), _TEXT(__FILE__), __LINE__, _TEXT(#_condition_)); \
+		engine::g_AssertLog->SetBehaviours(static_cast<engine::ILogFile::eBehaviourFlag>(engine::ILogFile::eBF_OutputToDebugger | engine::ILogFile::eBF_FlushEachWrite)); \
 		LogAssert(__VA_ARGS__); \
+		engine::g_AssertLog->SetBehaviours(behaviourFlags); \
 		DebugBreak(); \
 	}
 #endif
