f41afdbf7f6dd7795e443b09f34d8db7f5806a31
andyc <andyc@crytek.com>
2011-03-18 16:08:53 +0000

    added console app class

---
+++ engine/independent/application/consoleapplication.h
@@ -0,0 +1,35 @@
+#if !defined(__CONSOLEAPPLICATION_H__)
+#define __CONSOLEAPPLICATION_H__
+
+//==============================================================================
+
+#include "application/application.h"
+
+//==============================================================================
+
+namespace engine
+{
+	//============================================================================
+	// CConsoleApplication
+	//============================================================================
+	class CConsoleApplication : public CApplication
+	{
+		typedef CApplication PARENT;
+
+	public:
+		CConsoleApplication(void);
+		virtual ~CConsoleApplication(void);
+
+		virtual bool Update(void) { return PARENT::Update(); }
+
+	protected:
+		virtual bool	Update(double frameTime, uint32 frameCount);
+
+	private:
+	};
+
+} // End [namespace engine]
+
+//==============================================================================
+#endif // End [!defined(__CONSOLEAPPLICATION_H__)]
+// [EOF]
